af2d8675446d74030695ec7fe7f63754e58b0a8a
aldk98 <aldrian.kwantawijaya@binus.ac.id>
2018-09-30 00:23:50 +0700

    Moved and renamed ApiController to AuthController to Http/Controller/API Fix User model typo

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+use App\User;
+use Carbon\Carbon;
+use Validator;
+class AuthController extends Controller
+{
+   /**
+     * Create user
+     *
+     * @param  [string] name
+     * @param  [string] email
+     * @param  [string] password
+     * @param  [string] password_confirmation
+     * @return [string] message
+     */
+
+    public function register(Request $request)
+    {
+
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|confirmed'
+        ]);
+        $user = new User([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        $user->save();
+        return response()->json([
+            'message' => 'Successfully created user!'
+        ], 201);
+    }
+
+    /**
+     * Login
+     * @param  [string] email
+     * @param  [string] password
+     * @return [string] message
+     */
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+            'remember_me' => 'boolean'
+        ]);
+        $credentials = request(['email', 'password']);
+        if(!Auth::attempt($credentials)){
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+        }
+
+        $tokenResult = $user->createToken('Access Token');
+
+        $token = $tokenResult->token;
+
+        if ($request->remember_me){
+            $token->expires_at = Carbon::now()->addWeeks(4);
+        }
+        else{
+            $token->expires_at = Carbon::now()->addDays(1);
+        }
+
+
+        $token->save();
+        return response()->json([
+            'access_token' => $tokenResult->accessToken,
+            'token_type' => 'Bearer',
+            'expires_at' => Carbon::parse(
+                $tokenResult->token->expires_at
+            )->toDateTimeString()
+        ]);
+    }
+
+    /**
+     * Logout
+     *
+     * @return [string] message
+     */
+    public function logout(Request $request)
+    {
+        $request->user()->token()->revoke();
+        return response()->json([
+            'message' => 'Successfully logged out'
+        ]);
+    }
+
+    /**
+     * Get user
+     *
+     * @return [json] user object
+     */
+    public function user(Request $request)
+    {
+        return response()->json($request->user());
+    }
+}
